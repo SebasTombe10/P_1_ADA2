9def0db8d8f25d6acd7713e012b10f8fb0e7eca0
Sebastian Tombe Campo (202110051) <johan.tombe@correounivalle.edu.co>
2023-04-23 22:03:18 -0500

    Fuerza bruta implementado

--- app.py
+++ app.py
@@ -12,6 +12,7 @@ ventana.geometry('600x400')
 
 # Función para abrir un archivo de texto (.sub o .psub)
 def abrir_archivo():
+    global ruta_archivo
     # Definimos los tipos de archivo permitidos
     tipos_archivo = [('Archivos de psubasta', '*.sub'),('Archivos de subasta', '*.sub') ]
     # Abrimos el diálogo para seleccionar el archivo
@@ -30,12 +31,12 @@ def abrir_archivo():
              trio = Label(ventana, text="Acciones:"+ A +"Precio: "+ B + "Oferentes: " + n )
              trio.pack()
 
-             lineas = archivo.read().split('\n')
             #Imprimiendo los oferentes
-             print(lineas)
-             print(''.join(lineas))
-             for linea in lineas:
-                 if(contador_oferentes != len(lineas)):
+            #Imprimir lineas en pantalla 
+             print('abrir')
+             lineas = archivo.read().split('\n')
+             for linea in lineas[:-1]:
+                 if(contador_oferentes != len(lineas)-1):
                     oferentes = Label(ventana, text="Oferente("+ str(contador_oferentes)+")" +"-->" + "Oferta" +"("+''.join(linea)+")")
                     oferentes.pack()
                     contador_oferentes+=1
@@ -43,7 +44,73 @@ def abrir_archivo():
                      oferentes = Label(ventana, text="Gobierno: "+"-->" + "Oferta" +"("+''.join(linea)+")")
                      oferentes.pack()
 
+#funcion de fuerza bruta
+def mejor_oferente_fb():
+    oferente = []
+    contador_oferentes=0
+    ganancia_gobierno_fb=0
+    # Verificamos si se seleccionó un archivo
+    if ruta_archivo != '':
+        # Abrimos el archivo y lo imprimimos en la consola
+        with open(ruta_archivo, 'r') as archivo:
+             lineas = archivo.readlines()
+             #Obtenemos la informacion de: A - Acciones B - Precio C - numero de oferentes
+             A = int(lineas[0])
+             B = int(lineas[1])
+             n = int(lineas[2])
+             #print(lineas)
+             #Iniciamos la lista en el tamaño de oferentes 
+             oferentes = [None] * len(lineas[3:-1])
+             for linea in lineas[3:-1]:
+                #valores contiene cada oferentes p,r,c - NO INCLUYE GOBIERNO
+                valores = linea.split(',')
+                #Valores de los oferentes
+                #p=oferta del oferente
+                #r=maximo de acciones que compra
+                #c=minimo de acciones que compra
+                #respectivamente
+                oferente.append(int(valores[0]))
+                oferente.append(int(valores[1]))
+                oferente.append(int(valores[2]))
+                #print(len(lineas[3:]))
+                #cargamos la lista oferentes con todos los oferentes
+                #ecordando que por ultimo esta el gobierno y las acciones 
+                #no vendiadaas seran vendidas al gobierno
+                if(contador_oferentes != len(lineas[3:])-1):
+                    oferentes[contador_oferentes] = oferente
+                    oferente = []
+                    contador_oferentes+=1
 
+             #Ordenamos la lista de oferentes lexicograficamente
+             oferentes_lexicografico = sorted(oferentes,key=lambda x: x)
+             #contdor de cantidad de oferentes para poder saber a que oferentes se le vendio
+             contador_venta_of=0
+             #contamos las acciones disponibles durante la ejecucion
+             acciones_disponibles=A
+             #guardamos la ganancia del gobierno por cada venta
+    
+             for mejor_oferente in reversed(oferentes_lexicografico):
+                contador_venta_of+=1
+                #print('Precio al que cmpra: ' + str(mejor_oferente[0]))
+                #print('max: ' + str(mejor_oferente[1])) 
+                #print('min: ' + str(mejor_oferente[2]))
+                #Vendemos al primero de la lista y reducimos las accion que se vendieron 
+                if(acciones_disponibles != 0 and mejor_oferente[1] <= acciones_disponibles and mejor_oferente[1] != 0):
+                    #print(acciones_disponibles)
+                    acciones_disponibles = acciones_disponibles - mejor_oferente[1]
+                    ganancia_gobierno_fb += mejor_oferente[0] * mejor_oferente[1]
+                    venta_acciones = Label(ventana, text="Se vendieron: "+ str(mejor_oferente[1]) + "acciones" + "al oferente: (" + str(contador_venta_of) +") a un precio de: "+ str(mejor_oferente[0]))
+                    venta_acciones.pack()
+
+             if(acciones_disponibles != 0):
+                 ganancia_gobierno_fb+=acciones_disponibles*B
+                 venta_acciones_gob = Label(ventana, text="Se vendieron: "+ str(acciones_disponibles) + "acciones" + "al GOBIERNO a un precio de: "+ str(B))
+                 venta_acciones_gob.pack()
+    
+    ganancia_gob = Label(ventana, text="Las ganancias de goberno fueron: "+ str(ganancia_gobierno_fb))
+    ganancia_gob.pack()              
+
+             
 
 
 # Creamos el botón para abrir el archivo
@@ -54,6 +121,14 @@ boton_abrir = Button(
     cursor='X_cursor'
     )
 boton_abrir.pack()
+#Creamos boton para ejecutar fuerza bruta
+boton_mejor_fuerzabruta = Button(
+    ventana, 
+    text='Mejor con fuerza bruta', 
+    command=mejor_oferente_fb,
+    cursor='X_cursor'
+    )
+boton_mejor_fuerzabruta.pack()
 
 
 
